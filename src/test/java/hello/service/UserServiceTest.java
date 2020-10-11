0a9a9e4cdf3ab2ad8d632671c4a5c4b2fd822725
Ryan <2433005622@qq.com>
2020-10-11 16:46:45 +0800

    添加测试模块（mockito/junit）

---
+++ src/test/java/hello/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package hello.service;
+
+import hello.entity.User;
+import hello.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    BCryptPasswordEncoder mockEncoder;
+    @Mock
+    UserMapper mockMapper;
+    @InjectMocks
+    UserService userService;
+    @Test
+    public void testSave(){
+        //调用userService
+        //验证userService将请求转发给了userMapper
+
+        when(mockEncoder.encode("myPassword")).thenReturn("myEncodedPassword");
+
+        userService.save("myUsername","myPassword");
+
+        verify(mockMapper).save("myUsername","myEncodedPassword");
+    }
+    @Test
+    public void testGetUserByUsername(){
+        userService.getUserByUsername("myUsername");
+        verify(mockMapper).findUserByUsername("myUsername");
+    }
+    @Test
+    public void  throwExceptionWhenUserNotFound(){
+        when(mockMapper.findUserByUsername("myUsername")).thenReturn(null);
+
+        Assertions.assertThrows(UsernameNotFoundException.class,()->userService.loadUserByUsername("myUsername"));
+    }
+    @Test
+    public void  returnUserDetailsWhenUserFound(){
+        when(mockMapper.findUserByUsername("myUsername")).thenReturn(new User("myUsername","myEncryptedPassword"));
+        UserDetails userdetails = userService.loadUserByUsername("myUsername");
+        Assertions.assertEquals("myUsername",userdetails.getUsername());
+        Assertions.assertEquals("myEncryptedPassword",userdetails.getPassword());
+    }
+
+}
